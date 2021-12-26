f6c08bba8fdc35e49c01da8d7f9548c395a63467
Dali.wang <xdr94@126.com>
2021-12-26 22:15:24 +0800

    Update drawer.py
    access name simplify

--- components/drawer.py
+++ components/drawer.py
@@ -1,39 +1,41 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import networkx as nx
 class Drawer:
     def __init__(self):
         self.colors=['r','g','b','c','m','y','k']
-        self.fig = plt.figure(1)
+        self.access_names = ['s5012','s2618','s2518']
 
         pass
-    def get_handle(self):
-        return self.fig
-        # plt.show()
 
-        pass
 
 
 
     def __get_data_item__(self):
         pass
-    def __call__(self,data,config ):
+
+    def drawAer(self,data,config ):
+
+        fig = plt.figure(1)
         # time_arr = np.array(sub_df).T
         for sub_idx, col in enumerate(config['data_show_lines'][1:]): #different value lines
             plt.subplot(len(config['data_show_lines'])-1, 1, sub_idx + 1)  # without time
 
-            for idx, access_name in enumerate(data.access_names):# different access lines in a value
-
+            for idx, access_name in enumerate(self.access_names):# different access lines in a value
                 for line in data.get_sublines(access_name,[col],with_time=True):# different subline in a line
 
 
                     plt.plot(line.T[0],line.T[1],'{}'.format(self.colors[idx%(len(self.colors))]))
                 # print('idx: {}, mod:{}'.format(idx,idx%(len(self.colors))))
-            # plt.legend(data.access_names)
+            plt.legend(data.access_names)
             # plt.title(col)
-
+        return fig
         # plt.show()
             # line.set_drawstyle('_draw_steps_pre')
 
-        pass
 
+    def drawGraph(self,G):
+        fig = plt.figure(2)
+        nx.draw(G)
+        return fig
