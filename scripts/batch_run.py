1f6fcb50e98343e0b7a22b6e3785cb2aed763374
Dali.wang <xdr94@126.com>
2022-03-04 15:36:54 +0800

    Update batch_run.py

--- scripts/batch_run.py
+++ scripts/batch_run.py
@@ -24,70 +24,53 @@ def main(args):
     data.data_prep()
 
     # split data reload and process
-    data.load_align()
+    for seed in config['random_seed']:
+        try:
+            data.load_align()
+            stator = Stator(data)
 
-    stator = Stator(data)
-    stator.data_stat()
+            # stator.data_stat()
+        # drawer = Drawer(data,config)
 
-    data.data_parse()
-
-
-
-    print("\n=============PROBLEM=============")
-    solver = Solver(data)
-    solver.build_graph(weights='tk')
-
-    if config['algorithm']=='dp':
-
-        final_solution = solver.dp_run()
-    elif config['algorithm']=='mea':
-
-        final_solution = solver.mea_run()
-    else:
-        final_solution = solver.mst_run()
-
-
-    inter_tk_dict = solver.get_inter_tks(final_solution)
-    final_value = solver.get_selected_alg_base(inter_tk_dict,final_solution)
-    # solver.result_stat(final_solution,inter_tk_dict,final_value)
-
-    carrier = stator.solution_stat(final_solution,final_value)
-
-
-    print('\n============DRAW===============')
-
-    drawer = Drawer(data,config)
+            data.data_parse()
 
 
 
-    plt.figure(figsize=[18,8])
-    ax1=plt.subplot(2,3,4)
-    drawer.drawAer(ax1,position=data.position)
+        # print("\n=============PROBLEM=============")
+            solver = Solver(data)
+            for alg in config['algorithm']:
 
-    ax2=plt.subplot(2,3,5)
-    drawer.drawAerSolution(ax2,position=data.position,
-                                  final_solution=final_solution,
-                                  inter_tk_dict=inter_tk_dict
-                                  )
+                if alg=='dp':
+                    solver.build_graph(weights='tk')
 
-    plt.subplot(2, 3,6)
-    drawer.drawGraph(solver.G, position=data.position, final_solution=final_solution)
-    # figs = drawer.drawGraph(solver.G,position = data.position)
+                    final_solution = solver.dp_run()
+                elif alg=='mea':
 
-    ax4=plt.subplot(2, 1, 1)
-    drawer.drawAccessSolution(ax4,
-                                  position=data.position,
-                                  final_solution=final_solution,
-                                  inter_tk_dict=inter_tk_dict,
-                             )
+                    final_solution = solver.mea_run()
+                elif alg=='mst':
+                    solver.build_graph(weights='1')
 
-    print('============SAVE===============')
-    plt.savefig(instance_save_path/'solution.png')
+                    final_solution = solver.mst_run()
+                else:
+                    print('error in alg')
+                    exit(-1)
 
+
+                inter_tk_dict = solver.get_inter_tks(final_solution)
+                final_value = solver.get_selected_alg_base(inter_tk_dict,final_solution)
+                # solver.result_stat(final_solution,inter_tk_dict,final_value)
+
+                carrier = stator.solution_stat(final_solution,final_value,algorithm=alg)
+
+
+
+                dict2json(instance_save_path/'{}_{}_stat_results.json'.format(alg,seed),carrier)
+                to_csv(instance_save_path/'{}_{}_final_value.csv'.format(alg,seed),final_value)
+            del stator
+        except:
+            continue
+        # del solver
     yml.save_log(instance_save_path/'settings.yaml')
-    dict2json(instance_save_path/'stat_results.json',carrier)
-    to_csv(instance_save_path/'final_value.csv',final_value)
-    read_csv(instance_save_path/'final_value.csv')
 
 
     print('-> LOGFILE SAVED AS :{}'.format(instance_save_path))
@@ -95,9 +78,9 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="stk-conn")
     if os.path.exists('../configs/config.yaml'):
-        parser.add_argument("--settings", default='../configs/config.yaml')
+        parser.add_argument("--settings", default='../configs/batch_run_config.yaml')
     else:
-        parser.add_argument("--settings", default='./configs/config.yaml')
+        parser.add_argument("--settings", default='./configs/batch_run_config.yaml')
 
     args = parser.parse_args()
     main(args)
