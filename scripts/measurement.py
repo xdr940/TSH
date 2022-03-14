275b4f4d65a010160a162b52bfa864d65d608d72
Dali.wang <xdr94@126.com>
2022-03-14 21:30:02 +0800

    Update measurement.py

--- scripts/measurement.py
+++ scripts/measurement.py
@@ -9,6 +9,9 @@ import os
 from path import Path
 from dataset import ResultDataset,CallDataset
 from components.drawer import StatDrawer
+import numpy as np
+import pandas as pd
+import seaborn as sns
 from components.measurement import Measurer
 
 def main(args):
@@ -16,44 +19,51 @@ def main(args):
     yml = YamlHandler(args.settings)
     config = yml.read_yaml()
     data = ResultDataset(
-        instances_path = Path(config['instances_path']),
+        instances_path=Path(config['instances_path']),
         stems=config['stems'],
         ignore_dirs=config['ignore_dirs'],
         ignore_words=config['ignore_words']
     )
     data.load()
+    sim_duration = config['stems'][0][-4:]
 
-    call_set = CallDataset()
-    measurer = Measurer()
-    measurer.P_drop(data.total_precedures['0500'])
+    procedure_container = Measurer(data.total_precedures[sim_duration])
+    start_end_list = procedure_container.get_start_end()
+    call_container = CallDataset(start_end_list=start_end_list)
 
+    total_dfs = pd.DataFrame(columns=['start', 'end'], index=None)
+    for batch_procedure, batch_call in zip(
+            procedure_container.get_batch_procedure(),
+            call_container.get_batch_calls()
+    ):
+        value_vec = []#对么个batch 的procedure 的值记录
+        algs_name = [procedure.name[4:] for procedure in batch_procedure]
+        names_arry = []
+        batch_df = pd.DataFrame(columns=total_dfs.columns)
+        for procedure, name in zip(batch_procedure, algs_name):
+            vec, handover = procedure.inject(batch_call)
+            names_arry += ([name] * len(vec))
+            value_vec.append(vec)
+        batch_call_resize = np.concatenate([batch_call[:-1]] * len(algs_name))
+        batch_df[['start', 'end']] = batch_call_resize
+        batch_df['duration'] = batch_df['end'] - batch_df['start']
+        batch_df['value'] = np.concatenate(value_vec, axis=0)
+        batch_df['alg'] = names_arry
+        batch_df.index = range(len(total_dfs), len(total_dfs) + len(names_arry))
 
+        total_dfs = pd.concat([total_dfs, batch_df], axis=0)
 
+    sns.lineplot(data=total_dfs, x="duration", y="value", hue="alg", hue_order=['mst', 'mea', 'dp', 'gd'],
+                 palette=['r', 'g', 'b', 'y'])
+    plt.yscale("log")
+    plt.grid()
 
-    drawer = StatDrawer(data,config['palette'])
+    plt.ylabel("P_drop")
+    plt.xlabel("Call Duration (Sec)")
 
-    user_demands = data.create_user_demands()
-    # FIG1
-    plt.figure(figsize=(16,4))
-    plt.subplot(1,3,1)
-    drawer.draw_rssi()
-
-
-
-
-    #FIG 2 handover times
-    plt.subplot(1,3,2)
-
-    drawer.draw_num_handovers()
-
-
-
-    # FIG 3
-    plt.subplot(1,3,3)
-    drawer.draw_last_durations()
+    plt.yticks([0.1, 0.2, 0.5, 1])
 
     plt.show()
-
 
 
 
